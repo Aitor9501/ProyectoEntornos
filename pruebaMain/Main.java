33e8bc395377059408c76bdb70c6111cd94401b9
aitor <aitor9501@gmial.com>
2024-04-01 17:31:50 +0200

    Variable Cambiada

--- pruebaMain/Main.java
+++ pruebaMain/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Holi");
-        int n = 2;
+        int n = 5;
         System.out.println(n*2);
     }
 }
